11fadef686b5744342fcadc9286d970f1e5f0331
Suriyaa NS <suryansprof2004@gmail.com>
2026-01-14 01:05:42 +0530

    Time: 2 ms (8.84%) | Memory: 46.6 MB (31.59%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
@@ -6,13 +6,10 @@ class Solution {
         for(int i = 0;i<dim.length;i++){
                 x = Math.hypot(dim[i][0] , dim[i][1]);
                 int mul = dim[i][0]*dim[i][1];
-                if(x > d_len){
+                if(x > d_len || (x == d_len && mul > area)){
                     d_len = x;
                     area = mul;
-                } else if(x == d_len){
-                    if(mul > area)
-                        area = mul;
-                    }
+                }
         }
         return area;
     }
