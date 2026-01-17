d8890041e41c226878d048a8176af092ca3e8cf0
Suriyaa NS <suryansprof2004@gmail.com>
2026-01-17 22:27:09 +0530

    Time: 3 ms (97.02%) | Memory: 46.5 MB (30.07%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
@@ -1,21 +1,17 @@
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         int count = 0;
-        for (int i : arr1) {
-    boolean con = false;
-
-    for (int j : arr2) {
-        if (Math.abs(i - j) <= d) {
-            con = true;
-            break;
+        for(int i : arr1){
+            boolean con = false;
+            for(int j : arr2){
+                if(Math.abs(i-j)<=d){
+                    con = true;
+                    break;
+                }
+            }
+            if(!con)
+            count++;
         }
-    }
-
-    if (!con) {
-        count++;
-    }
-}
-
     return count;
     }
 }
