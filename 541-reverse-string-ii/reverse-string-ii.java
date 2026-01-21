a1290e190abed46c107cc6dde274062482bc591d
Suriyaa NS <suryansprof2004@gmail.com>
2026-01-22 02:16:59 +0530

    Time: 4 ms (9.49%) | Memory: 44.7 MB (85.51%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -4,10 +4,7 @@ class Solution {
 
         for(int i = 0;i<s.length();i=i+2*k){
             int l = i;
-            int r = i+k-1;
-            if(r>=s.length()){
-                r = s.length() -1;
-            }
+            int r = Math.min(i+k-1,s.length()-1);
         while(l<r){
             char temp = s.charAt(l);
             s.setCharAt(l,s.charAt(r));
