55f4ac4ce687e2f5d60718cd8c88f14785298033
Suriyaa NS <suryansprof2004@gmail.com>
2026-01-22 02:17:23 +0530

    Time: 4 ms (9.49%) | Memory: 45 MB (38.24%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -2,16 +2,19 @@ class Solution {
     public String reverseStr(String str, int k) {
         StringBuilder s = new StringBuilder(str);
 
-        for(int i = 0;i<s.length();i=i+2*k){
+        for(int i = 0;i<s.length();i=2*k+i){
             int l = i;
-            int r = Math.min(i+k-1,s.length()-1);
+            int r = i+k-1;
+            if(r>=s.length()){
+                r = s.length() -1;
+            }
         while(l<r){
             char temp = s.charAt(l);
             s.setCharAt(l,s.charAt(r));
             s.setCharAt(r,temp);
             l++;
             r--;
-            }
+        }
         }
     return s.toString();
     }
