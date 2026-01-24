945e09807f119fe9ea615ce5413c1e46c5dd32f5
Suriyaa NS <suryansprof2004@gmail.com>
2026-01-24 23:21:35 +0530

    Time: 3 ms (71.63%) | Memory: 44.7 MB (43.81%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        //array to arraylist
+        List<Integer> list = new ArrayList<>();
+        for(int i : nums){
+            list.add(i);
+        }
+        if(sorted(list)){
+            return 0;
+        }
+        int count = 0;
+        boolean isOrder = false;
+        //iterate and find the smalest pair
+        //iterate reverse , so even if multiple pair exist , we choose left most one
+
+        while(!isOrder){
+        int index = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i = 1;i<list.size();i++){
+            if(list.get(i) + list.get(i-1) < min ){
+                min = list.get(i) + list.get(i-1);
+                index = i;
+            }
+
+        }
+        list.remove(index);
+        list.remove(index-1);
+        list.add(index-1,min);
+
+        isOrder = sorted(list);
+        count++;
+        }
+        return count;
+
+    }
+
+    //check if array is sorted or not
+    boolean sorted(List<Integer> list){
+        boolean isOrder = true;
+        for(int i = 0;i<list.size()-1;i++){
+            if(list.get(i) > list.get(i+1)){
+                isOrder = false;
+                break;
+            }
+        }
+    return isOrder;
+    }
+}
