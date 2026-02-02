df706113b538d6aecfc06e0922368b06c8a39eeb
Suriyaa NS <suryansprof2004@gmail.com>
2026-02-02 23:29:37 +0530

    Time: 1290 ms (5.03%) | Memory: 51.8 MB (7.89%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> list = new ArrayList<>();
+        for(char i : s.toCharArray()){
+            if("AEIOUaeiou".contains(String.valueOf(i))){
+            list.add(i);
+            }
+        }
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for( char i : s.toCharArray()){
+            if("AEIOUaeiou".contains(String.valueOf(i))){
+                sb.append(list.get(0));
+                list.remove(0);
+            }
+            else{
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
+}
