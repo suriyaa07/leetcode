7635fe048084053383a6a97236e851ca16941c1b
Suriyaa NS <suryansprof2004@gmail.com>
2026-02-09 22:44:07 +0530

    Time: 1 ms (86.12%) | Memory: 42.8 MB (24.08%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxBottlesDrunk(int bottle, int rate) {
+        int drank = bottle;
+        int empty = bottle;
+
+        while(empty >= rate){
+            empty = empty - rate;
+            drank++;
+            empty++;
+            rate++;
+        }
+        return drank;
+    }
+}
