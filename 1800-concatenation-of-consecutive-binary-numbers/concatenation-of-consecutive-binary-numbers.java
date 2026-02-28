c6d4cf1d59f990912fd9e720c0dd7902a56db930
Suriyaa NS <suryansprof2004@gmail.com>
2026-02-28 21:11:03 +0530

    Time: 646 ms (17.44%) | Memory: 103.5 MB (6.98%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -1,13 +1,11 @@
 class Solution {
     public int concatenatedBinary(int n) {
-        //making a string of binary numbers from 1 to n
         StringBuilder sb = new StringBuilder();
         for(int i = 1;i<=n;i++){
             String binary = Integer.toBinaryString(i);
             sb.append(binary);
         }
         String finalBinary = sb.toString();
-        //converting binary to decimal
         long ans = 0;
         for(int i = 0;i<finalBinary.length();i++){
             int num = finalBinary.charAt(i) - '0';
