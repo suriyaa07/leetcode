f1d026da9997662bc91fa6af21c4a04708336e00
Suriyaa NS <suryansprof2004@gmail.com>
2026-03-08 09:49:13 +0530

    Time: 3 ms (56.65%) | Memory: 46.6 MB (34.30%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        List<String> set = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
         List<String> list = new ArrayList<>();
-        set.add(words[0]);
-
+        temp.add(words[0]);
         for(int i = 1;i<words.length;i++){
             char[] arr1 = words[i].toCharArray();
             Arrays.sort(arr1);
             String word2 = new String(arr1);
-            String last = set.getLast();
+            String last = temp.getLast();
             char[] arr2 = last.toCharArray();
             Arrays.sort(arr2);
             String word1 = new String(arr2);
             if(!word1.equals(word2)){
-                set.add(words[i]);
+                temp.add(words[i]);
             }
         }
 
-        for(String x : set){
+        for(String x : temp){
             list.add(x);
         }
 
