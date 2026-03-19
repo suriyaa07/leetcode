cef1e0e32b0984afcc5dfbe8965d72d89e031327
Suriyaa NS <suryansprof2004@gmail.com>
2026-03-20 01:55:52 +0530

    Time: 1 ms (4.40%) | Memory: 46.5 MB (75.57%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.java
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -10,14 +10,12 @@
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
         
-        // find the largest element and its index
         int len = mountainArr.length();
         int start = 0;
         int end = len -1;
 
         while(start<end){
             int mid = start+(end-start)/2;
-
             if(mountainArr.get(mid) < mountainArr.get(mid+1))
                 start = mid+1;
             else
@@ -29,29 +27,29 @@ class Solution {
         //lets have a count element
         //now search for target in 0 to peak , peak to end
         int peak = start;
-        int start1 = 0;
-        int end1 = peak;
-        while(start1<=end1){
-            int mid1 = start1+(end1-start1)/2;
+        start = 0;
+        end = peak;
+        while(start<=end){
+            int mid1 = start+(end-start)/2;
 
             if(mountainArr.get(mid1) < target)
-                start1 = mid1+1;
+                start = mid1+1;
             else if(mountainArr.get(mid1) > target)
-                end1 = mid1 - 1;
+                end = mid1 - 1;
             else{
                 return mid1;
             }
         }
 
-        int start2 = peak;
-        int end2 = len -1;
-        while(start2<=end2){
-            int mid2 = start2+(end2-start2)/2;
+        start = peak;
+        end = len -1;
+        while(start<=end){
+            int mid2 = start+(end-start)/2;
 
             if(mountainArr.get(mid2) > target)
-                start2 = mid2+1;
+                start = mid2+1;
             else if(mountainArr.get(mid2) < target)
-                end2 = mid2 - 1;
+                end = mid2 - 1;
             else{
                 return mid2;
             }
