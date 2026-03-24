17a376952ffc6c4c0fa15a420ba44b416657b260
Suriyaa NS <suryansprof2004@gmail.com>
2026-03-25 00:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (77.28%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,37 +1,6 @@
 class Solution {
     public boolean check(int[] nums) {
         int len = nums.length;
-
-        
-        // int pivot = -1;
-        // for(int i = 0; i < len - 1; i++){
-        //     if(nums[i] > nums[i+1]){
-        //         pivot = i;
-        //         break;
-        //     }
-        // }
-
-        // List<Integer> list = new ArrayList<>();
-
-        // if(pivot == -1){
-        //     for(int num : nums){
-        //         list.add(num);
-        //     }
-        // } else {
-        // for(int i = pivot+1; i < len; i++){
-        //     list.add(nums[i]);
-        // }
-        // for(int i = 0; i <= pivot; i++){
-        //     list.add(nums[i]);
-        // }
-        // }
-        // Arrays.sort(nums);
-
-        // for(int i = 0; i < len; i++){
-        //     if(nums[i] != list.get(i)){
-        //         return false;
-        //     }
-        // }
         int count = 0;
         for(int i = 0;i<len;i++){
             if(nums[i]>nums[(i+1)%len])
@@ -41,6 +10,5 @@ class Solution {
             return false;
 
         return true;
-        
     }
 }
