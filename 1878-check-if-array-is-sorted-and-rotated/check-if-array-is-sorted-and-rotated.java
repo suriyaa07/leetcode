e11c4396dad229d73b7f1601c3d2a50fc0064677
Suriyaa NS <suryansprof2004@gmail.com>
2026-03-25 00:16:38 +0530

    Time: 4 ms (3.18%) | Memory: 43.2 MB (21.89%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int len = nums.length;
+
+        
+        int pivot = -1;
+        for(int i = 0; i < len - 1; i++){
+            if(nums[i] > nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        if(pivot == -1){
+            for(int num : nums){
+                list.add(num);
+            }
+        } else {
+        for(int i = pivot+1; i < len; i++){
+            list.add(nums[i]);
+        }
+        for(int i = 0; i <= pivot; i++){
+            list.add(nums[i]);
+        }
+        }
+        Arrays.sort(nums);
+
+        for(int i = 0; i < len; i++){
+            if(nums[i] != list.get(i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
