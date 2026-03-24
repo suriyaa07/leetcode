00462c67bb08728b1a2342469d30375838f564f7
Suriyaa NS <suryansprof2004@gmail.com>
2026-03-25 00:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (77.28%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,36 +3,44 @@ class Solution {
         int len = nums.length;
 
         
-        int pivot = -1;
-        for(int i = 0; i < len - 1; i++){
-            if(nums[i] > nums[i+1]){
-                pivot = i;
-                break;
-            }
-        }
+        // int pivot = -1;
+        // for(int i = 0; i < len - 1; i++){
+        //     if(nums[i] > nums[i+1]){
+        //         pivot = i;
+        //         break;
+        //     }
+        // }
 
-        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
 
-        if(pivot == -1){
-            for(int num : nums){
-                list.add(num);
-            }
-        } else {
-        for(int i = pivot+1; i < len; i++){
-            list.add(nums[i]);
-        }
-        for(int i = 0; i <= pivot; i++){
-            list.add(nums[i]);
-        }
-        }
-        Arrays.sort(nums);
+        // if(pivot == -1){
+        //     for(int num : nums){
+        //         list.add(num);
+        //     }
+        // } else {
+        // for(int i = pivot+1; i < len; i++){
+        //     list.add(nums[i]);
+        // }
+        // for(int i = 0; i <= pivot; i++){
+        //     list.add(nums[i]);
+        // }
+        // }
+        // Arrays.sort(nums);
 
-        for(int i = 0; i < len; i++){
-            if(nums[i] != list.get(i)){
-                return false;
-            }
+        // for(int i = 0; i < len; i++){
+        //     if(nums[i] != list.get(i)){
+        //         return false;
+        //     }
+        // }
+        int count = 0;
+        for(int i = 0;i<len;i++){
+            if(nums[i]>nums[(i+1)%len])
+                count++;
         }
+        if(count>1)
+            return false;
 
         return true;
+        
     }
 }
