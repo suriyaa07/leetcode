a49f569d9598f5b5da2e9c5be03a80fc2d90229b
Suriyaa NS <suryansprof2004@gmail.com>
2026-04-02 02:02:32 +0530

    Time: 2 ms (70.41%) | Memory: 42.7 MB (73.06%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,26 +1,36 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int index = 0;
-        int sLen = s.length();
-        int tLen = t.length();
+        // int index = 0;
+        // int sLen = s.length();
+        // int tLen = t.length();
 
-        for(int i = 0;i<sLen;i++){
-            char ch = s.charAt(i);
-            // StringBuilder sb = new StringBuilder();
-            String sub = t.substring(index,tLen);
-            System.out.println(sub);
-            boolean found = false;
-            for(int j = 0;j<sub.length();j++){
-                if(sub.charAt(j)==ch){
-                    found = true;
-                    index = index + j + 1;
-                    break;
-                }
+        // for(int i = 0;i<sLen;i++){
+        //     char ch = s.charAt(i);
+        //     // StringBuilder sb = new StringBuilder();
+        //     String sub = t.substring(index,tLen);
+        //     System.out.println(sub);
+        //     boolean found = false;
+        //     for(int j = 0;j<sub.length();j++){
+        //         if(sub.charAt(j)==ch){
+        //             found = true;
+        //             index = index + j + 1;
+        //             break;
+        //         }
+        //     }
+        //     if(!found){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int i = 0, j = 0;
+
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
             }
-            if(!found){
-                return false;
-            }
+            j++;
         }
-        return true;
+        return i == s.length();
     }
 }
