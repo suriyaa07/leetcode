b59f6dcb120b3a93f4d2289f00c2e8b6172e034a
Suriyaa NS <suryansprof2004@gmail.com>
2026-05-22 11:15:28 +0530

    Time: 7 ms (24.65%) | Memory: 46.7 MB (24.94%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -5,13 +5,12 @@ class Solution {
         for(int i : nums){
             String n = "" + i;
             for(int j = 0;j<n.length();j++){
-                Integer x = Integer.valueOf( String.valueOf(n.charAt(j)) );
+                Integer x = Integer.valueOf( n.charAt(j)-'0' );
                 list.add(x);
             }
         }
 
-        int len = list.size();
-        int[] ans = new int[len];
+        int[] ans = new int[list.size()];
         int j = 0;
         for(int i : list){
             ans[j] = i;
